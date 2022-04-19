57a506a3c1a1e417cc83e6156db2a38fe41e6782
Marco Cattaneo <marcocatta@gmail.com>
2022-04-19 20:50:02 +0200

    feat: changed icon for playing track

--- app/src/main/java/dev/marcocattaneo/sleep/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/dev/marcocattaneo/sleep/ui/screen/home/HomeScreen.kt
@@ -108,10 +108,11 @@ private fun MediaItem(
             )
             .then(modifier)
     ) {
+        val selected = mediaFile?.selected == true
         Icon(
-            painter = painterResource(id = R.drawable.ic_baseline_play_circle_outline_24),
+            painter = painterResource(id = if (selected) R.drawable.ic_baseline_play_circle_filled_24 else R.drawable.ic_baseline_play_circle_outline_24),
             contentDescription = mediaFile?.name ?: "Undefined",
-            tint = if (mediaFile?.selected == true) MaterialTheme.colors.primary else MaterialTheme.colors.secondary
+            tint = if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.secondary
         )
         Spacer8()
         Column {
